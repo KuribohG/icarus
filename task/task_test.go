ac37492ebaec5c07d20215318dddfd0fba225fa0
dyf <dyf@megvii.com>
2015-07-22 23:32:41 +0800

    Task: More tests

--- task/task_test.go
+++ task/task_test.go
@@ -11,10 +11,12 @@ import (
 )
 
 type testCourse struct {
-	remaining   int32
-	errorToMake int32
-	elected     int32
-	concurrent  int32
+	remaining    int32
+	errorToMake  int32
+	elected      int32
+	concurrent   int32
+	noConcurrent bool
+	forbidden    bool
 }
 
 const session = "xxx"
@@ -38,10 +40,14 @@ func (t *testCourse) Description() string {
 
 func (t *testCourse) Elect(k LoginSession) (bool, error) {
 	conc := atomic.AddInt32(&t.concurrent, 1)
-	if conc > 1 {
+	if conc > 1 && t.noConcurrent {
 		log.Fatalf("2 goroutines electing at the same time!")
 	}
 	defer atomic.AddInt32(&t.concurrent, -1)
+
+	if t.forbidden {
+		log.Fatalf("Entered forbidden elect function.")
+	}
 
 	log.Printf("Electing...Remaining %d, Elected %d", t.remaining, t.elected)
 	if reflect.TypeOf(k).Name() != "string" {
@@ -61,15 +67,18 @@ func (t *testCourse) Elect(k LoginSession) (bool, error) {
 			atomic.AddInt32(&t.remaining, -1)
 			return false, nil
 		} else {
+			log.Println("Elected!")
 			return true, nil
 		}
 	}
 }
 
 type testUser struct {
-	errorToMake int32
-	loginCount  int32
-	concurrent  int32
+	errorToMake  int32
+	loginCount   int32
+	concurrent   int32
+	noConcurrent bool
+	forbidden    bool
 }
 
 func (t *testUser) Name() string {
@@ -78,10 +87,14 @@ func (t *testUser) Name() string {
 
 func (t *testUser) Login() (LoginSession, error) {
 	conc := atomic.AddInt32(&t.concurrent, 1)
-	if conc > 1 {
+	if conc > 1 && t.noConcurrent {
 		log.Fatalf("2 goroutines electing at the same time!")
 	}
 	defer atomic.AddInt32(&t.concurrent, -1)
+
+	if t.forbidden {
+		log.Fatalf("Entered forbidden login function.")
+	}
 
 	log.Printf("Login...")
 	atomic.AddInt32(&t.loginCount, 1)
@@ -97,6 +110,7 @@ func (t *testUser) Login() (LoginSession, error) {
 func TestBasic(t *testing.T) {
 	MaxRetry = 5
 	LoopInterval = 3 * time.Second
+	log.Printf("TestBasic: Elect every %.2f seconds.", float32(LoopInterval/time.Second))
 
 	var testCount int32 = 5
 
@@ -112,7 +126,7 @@ func TestBasic(t *testing.T) {
 	t.Logf("User: ER = %d, L = %d\n", user.errorToMake, user.loginCount)
 	t.Logf("Course: R = %d, ER = %d, EL = %d\n", course.remaining, course.errorToMake, course.elected)
 
-	if user.loginCount != 1 {
+	if user.loginCount > 1 {
 		t.Fatalf("Login for %d time(s).", user.loginCount)
 	}
 	if course.elected != testCount+1 {
@@ -126,5 +140,118 @@ func TestBasic(t *testing.T) {
 	}
 	if !elected {
 		t.Fatalf("Should elected.")
+	}
+}
+
+func TestStartStop(t *testing.T) {
+	MaxRetry = 5
+	LoopInterval = 3 * time.Second
+	log.Printf("TestStartStop: Elect every %.2f seconds.", float32(LoopInterval/time.Second))
+
+	var testCount int32 = 5
+
+	user := testUser{}
+	course := NewTestCourse(testCount, 0)
+	testTask := NewTask(&user, []Course{&course})
+	testTask.Start()
+	time.Sleep(time.Duration(rand.Float32()*100) * time.Millisecond)
+	testTask.Stop()
+	time.Sleep(time.Duration(rand.Float32()*100) * time.Millisecond)
+	testTask.Start()
+	time.Sleep(time.Duration(2) * LoopInterval)
+	course.noConcurrent = true
+	user.noConcurrent = true
+	time.Sleep(time.Duration(testCount+2) * LoopInterval)
+
+	running, succeeded, failed, lastError, elected := testTask.Statistics()
+	t.Logf("R = %t, S/F = %d/%d, LE = \"%s\", E = %t\n", running, succeeded, failed, lastError, elected)
+
+	t.Logf("User: ER = %d, L = %d\n", user.errorToMake, user.loginCount)
+	t.Logf("Course: R = %d, ER = %d, EL = %d\n", course.remaining, course.errorToMake, course.elected)
+
+	if user.loginCount > 2 {
+		t.Fatalf("Login for %d time(s).", user.loginCount)
+	}
+	if course.elected != testCount+1 {
+		t.Fatalf("Elected for %d time(s).", course.elected)
+	}
+	if course.remaining != 0 {
+		t.Fatalf("WTF...Remaining %d != 0.", course.remaining)
+	}
+	if running {
+		t.Fatalf("Still running.")
+	}
+	if !elected {
+		t.Fatalf("Should elected.")
+	}
+}
+
+func TestRestart(t *testing.T) {
+	MaxRetry = 5
+	LoopInterval = 3 * time.Second
+	log.Printf("TestRestart: Elect every %.2f seconds.", float32(LoopInterval/time.Second))
+
+	var testCount int32 = 5
+	var errorToMake int32 = 10
+
+	user := testUser{}
+	course := NewTestCourse(testCount, 0)
+	course.errorToMake = errorToMake
+	testTask := NewTask(&user, []Course{&course})
+	testTask.Start()
+	time.Sleep(time.Duration(testCount+errorToMake+5) * LoopInterval)
+
+	running, succeeded, failed, lastError, elected := testTask.Statistics()
+	t.Logf("R = %t, S/F = %d/%d, LE = \"%s\", E = %t\n", running, succeeded, failed, lastError, elected)
+
+	t.Logf("User: ER = %d, L = %d\n", user.errorToMake, user.loginCount)
+	t.Logf("Course: R = %d, ER = %d, EL = %d\n", course.remaining, course.errorToMake, course.elected)
+
+	if user.loginCount > 3 {
+		t.Fatalf("Login for %d time(s).", user.loginCount)
+	}
+	if course.elected != testCount+errorToMake+1 {
+		t.Fatalf("Elected for %d time(s).", course.elected)
+	}
+	if course.remaining != 0 {
+		t.Fatalf("WTF...Remaining %d != 0.", course.remaining)
+	}
+	if running {
+		t.Fatalf("Still running.")
+	}
+	if !elected {
+		t.Fatalf("Should elected.")
+	}
+}
+
+func TestStop(t *testing.T) {
+	MaxRetry = 5
+	LoopInterval = 3 * time.Second
+	log.Printf("TestRestart: Elect every %.2f seconds.", float32(LoopInterval/time.Second))
+
+	var testCount int32 = 5
+	user := testUser{}
+	course := NewTestCourse(testCount, 0)
+	testTask := NewTask(&user, []Course{&course})
+	testTask.Start()
+	time.Sleep(time.Duration(2) * LoopInterval)
+
+	log.Println("Stop!")
+	testTask.Stop()
+	time.Sleep(time.Duration(2) * LoopInterval)
+	course.forbidden = true
+	user.forbidden = true
+
+	running, succeeded, failed, lastError, elected := testTask.Statistics()
+	t.Logf("R = %t, S/F = %d/%d, LE = \"%s\", E = %t\n", running, succeeded, failed, lastError, elected)
+
+	t.Logf("User: ER = %d, L = %d\n", user.errorToMake, user.loginCount)
+	t.Logf("Course: R = %d, ER = %d, EL = %d\n", course.remaining, course.errorToMake, course.elected)
+
+	if running {
+		t.Fatalf("Still running.")
+	}
+	if elected {
+		t.Fatalf("Should not elected.")
 	}
 }
